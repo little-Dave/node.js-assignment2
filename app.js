76c976a72f1f5f8696dafa94a36bf8ee446d3e7c
little-Dave <17536424+little-Dave@users.noreply.github.com>
2020-06-12 21:11:01 -0600

    add routing
    send route-specific responses

--- app.js
+++ app.js
@@ -2,15 +2,23 @@ const express = require("express");
 
 const app = express();
 
-app.use((request, response, next) => {
-  console.log("Middleware1");
-  next();
+app.use("/users", (request, response, next) => {
+  response.send("<p>there are <span style='color: red;'>0</span> users to display</p>");
 });
 
-app.use((request, response, next) => {
-  console.log("Middleware2 (much cooler than 'Middleware1'.)");
-  response.send("<h1>Welcome to the PaRtY</h1>");
-  next();
+app.use("/", (request, response, next) => {
+  response.send("<p>hello world</p>");
 });
+
+// app.use((request, response, next) => {
+//   console.log("Middleware1");
+//   next();
+// });
+
+// app.use((request, response, next) => {
+//   console.log("Middleware2 (much cooler than 'Middleware1')");
+//   response.send("<h1>Welcome to the PaRtY</h1>");
+//   next();
+// });
 
 app.listen(3000);
